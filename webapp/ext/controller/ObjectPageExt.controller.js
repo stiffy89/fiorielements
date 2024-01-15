9470a9470b302ad1f20ed6a783c78d00167b4b9e
Steven Cao <steven.cao@ernox.com>
2024-01-15 07:21:06 +0000

    custom parts added

--- webapp/ext/controller/ObjectPageExt.controller.js
+++ webapp/ext/controller/ObjectPageExt.controller.js
@@ -1,11 +1,20 @@
 sap.ui.define([
-    "sap/m/MessageToast"
-], function(MessageToast) {
+    "sap/m/MessageToast",
+    "sap/ui/core/mvc/ControllerExtension"
+], function(MessageToast, ControllerExtension) {
     'use strict';
 
-    return {
+    return ControllerExtension.extend('ns.fiorielements.ext.controller.ObjectPageExt', {
+        
         Delete: function(oEvent) {
             MessageToast.show("Custom handler invoked.");
+        },
+        DateFormatter: function (source) {
+            if (source){
+                return new Date(source).toDateString();
+            } else {
+                return source
+            }
         }
-    };
+    }) 
 });
